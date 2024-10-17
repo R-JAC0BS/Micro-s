f18f16516e72f08d2c2f44e4ac115a2373f66eba
Roberto <1135584@atitus.edu.br>
2024-10-17 15:18:48 -0300

    new

---
+++ Saudacao-service/src/main/java/br/edu/atitus/microservicesconfigservice/saudacaoservice/SaudacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.microservicesconfigservice.saudacaoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudacaoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaudacaoServiceApplication.class, args);
+    }
+
+}
